0b13ec2b275ec10835646b68a8201476ced01960
Kelaib bin Mada <adambialek86@gmail.com>
2019-10-26 01:30:36 +0200

    Bookshelf server created

--- src/main/java/com/example/bookshelf/BookshelfApp.java
+++ src/main/java/com/example/bookshelf/BookshelfApp.java
@@ -1,4 +1,22 @@
 package com.example.bookshelf;
 
-public class BookshelfApp {
+import fi.iki.elonen.NanoHTTPD;
+
+import java.io.IOException;
+
+public class BookshelfApp extends NanoHTTPD {
+
+    public BookshelfApp(int port) throws IOException {
+        super(port);
+        start(5000,false);
+        System.out.println("Server has been started.");
+    }
+
+    public static void main(String[] args) {
+        try {
+            new BookshelfApp(8080);
+        } catch (IOException e) {
+            System.err.println("Server cannot be started because of error in:\n"+e);
+        }
+    }
 }
