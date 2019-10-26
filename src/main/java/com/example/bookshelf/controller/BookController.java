7e419826ed976a016e60888f4050efd6ba5f9d83
Kelaib bin Mada <adambialek86@gmail.com>
2019-10-26 03:12:20 +0200

    Add implementation of method getAllBooks

--- src/main/java/com/example/bookshelf/controller/BookController.java
+++ src/main/java/com/example/bookshelf/controller/BookController.java
@@ -2,7 +2,13 @@ package com.example.bookshelf.controller;
 
 import com.example.bookshelf.storage.BookStorage;
 import com.example.bookshelf.storage.impl.StaticListBookStorageImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fi.iki.elonen.NanoHTTPD;
+
+import static fi.iki.elonen.NanoHTTPD.Response.Status.INTERNAL_ERROR;
+import static fi.iki.elonen.NanoHTTPD.Response.Status.OK;
+import static fi.iki.elonen.NanoHTTPD.newFixedLengthResponse;
 
 
 public class BookController {
@@ -14,7 +20,16 @@ public class BookController {
     }
 
     public NanoHTTPD.Response serveGetBooksRequest(NanoHTTPD.IHTTPSession session) {
-        return null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        String response = "";
+
+        try {
+            response=objectMapper.writeValueAsString(bookStorage.getAllBooks());
+        } catch (JsonProcessingException e) {
+            System.err.println("Error during process request: \n"+e);
+            return newFixedLengthResponse(INTERNAL_ERROR,"text/plain","Internal error can't read all the books");
+        }
+        return newFixedLengthResponse(OK,"application/json",response);
     }
 
     public NanoHTTPD.Response serveAddBookRequest(NanoHTTPD.IHTTPSession session) {
